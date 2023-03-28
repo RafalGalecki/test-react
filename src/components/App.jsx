d3db5cf8c57f597ff8e4efe7d4c653dc1f225f0b
Rafał Gałecki <darthunter@gmail.com>
2023-03-28 10:44:27 +0200

    Button Icreasing Value

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+// import { useState, useEffect } from 'react';
+import ButtonIncreasingValue from './Button/Button';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      {/* <p>You clicked {value} times</p> */}
+      <ButtonIncreasingValue />
     </div>
   );
 };
