dd514071b6203749b3c7b3a11c9c80bdf11ecb2f
Rafał Gałecki <darthunter@gmail.com>
2023-04-13 17:09:18 +0200

    it works

--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,10 +1,18 @@
 import { Button } from 'components/Button/Button';
 import css from './TaskForm.module.css';
+import { useDispatch } from 'react-redux';
+import { addTask } from 'redux/actions';
 
 export const TaskForm = () => {
+  // Otrzymujemy odnośnik do funkcji wysyłania
+  const dispatch = useDispatch();
+
   const handleSubmit = event => {
     event.preventDefault();
     const form = event.target;
+    // Wywołujemy generator akcji i przekazujemy tekst zadania dla payload
+    // Wysyłamy wynik – akcję utworzenia zadania
+    dispatch(addTask(form.elements.text.value));
     form.reset();
   };
 
