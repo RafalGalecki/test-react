191fa1f7088072278d2f44dd62169a33d92eb6d7
Rafał Gałecki <darthunter@gmail.com>
2023-04-14 14:26:38 +0200

    comments deleted

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,31 +8,3 @@ export const store = configureStore({
   },
 });
 
-// Without redux-toolkit:
-// import { createStore } from "redux";
-// import { devToolsEnhancer } from "@redux-devtools/extension";
-// import { rootReducer } from './reducer';
-
-// // // Początkowa wartość statusu Redux dla Reducera root,
-// // // jeżeli nie przekaże się parametru preloadedState.
-// // const initialState = {
-// //   tasks: [
-// //     { id: 0, text: "Learn HTML and CSS", completed: true },
-// //     { id: 1, text: "Get good at JavaScript", completed: true },
-// //     { id: 2, text: "Master React", completed: false },
-// //     { id: 3, text: "Discover Redux", completed: false },
-// //     { id: 4, text: "Build amazing apps", completed: false },
-// //   ],
-// //   filters: {
-// //     status: "all",
-// //   },
-// // };
-// // // Tymczasem wykorzystujemy reducer, który
-// // // zwraca tylko otrzymany status
-// // const rootReducer = (state = initialState, action) => {
-// //   return state;
-// // };
-
-// const enhancer = devToolsEnhancer();
-
-// export const store = createStore(rootReducer, enhancer);
